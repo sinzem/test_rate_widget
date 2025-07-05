7dd1ecdad8f964149ac7f507f62c9fd43d25c4cb
sinzem <sinzem@gmail.com>
2025-07-05 18:35:24 +0300

    fix_date

--- app/script.js
+++ app/script.js
@@ -319,7 +319,7 @@ function dateNormalizeFromZoho(str) {
     const day = pad(date.getDate());
     const month = pad(date.getMonth() + 1); 
     const year = date.getFullYear();
-    const hours = +pad(date.getHours());
+    const hours = pad(date.getHours());
     const minutes = pad(date.getMinutes());
 
     return `${day}.${month}.${year} ${hours}:${minutes}`;
